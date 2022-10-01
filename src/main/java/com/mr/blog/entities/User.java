932f91752567c9823ec70ca4bfb1519c486a897f
Jose Carlos <josecarloscjj@gmail.com>
2022-10-01 16:34:47 -0300

    Created User entity

---
+++ src/main/java/com/mr/blog/entities/User.java
@@ -0,0 +1,70 @@
+package com.mr.blog.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 60)
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private LocalDate birthDate;
+    private String urlImage;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, LocalDate birthDate, String urlImage) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.urlImage = urlImage;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
