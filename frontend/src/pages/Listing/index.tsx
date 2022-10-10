49d85c620195b901f20be9db7819211e28a3fbe2
Jose Carlos <josecarloscjj@gmail.com>
2022-10-10 19:49:41 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,13 +1,27 @@
+import axios from "axios";
 import BlogCard from 'components/BlogCard';
 import Pagination from 'components/Pagination';
+import { useEffect, useState } from 'react';
+import { BlogPage } from "types/blog";
 import'./styles.css';
 
 function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`$BASE_URL}/posts/posts?size=10&page=1&sort=title`)
+    .then(response => {
+      const data = response.data as BlogPage;
+      console.log(data);
+      setPageNumber(data.number);
+    });
+  },[]);
 
     return (
       <><h1 className="news-title">Últimas notícias</h1>
       <section id="news">
         <BlogCard />
+        <p>{pageNumber}</p>
         <Pagination />
       </section></>
     );
