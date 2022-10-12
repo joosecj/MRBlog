a04ebbb472da64ff1bc5f4f2c0138df4cfd4ee3a
Jose Carlos <josecarloscjj@gmail.com>
2022-10-11 22:22:17 -0300

    Styling NavBar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,10 +1,13 @@
 import './styles.css';
+import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
     <header>
     <nav className="container">
-      <a className="nav-brand" href="/">MRBlog</a>
+    <Link to='/'> 
+      <div className="nav-brand">MRBlog</div>
+    </Link>
       <a className="menu-items" href="#news">Notícias</a>
     </nav>
     </header>
