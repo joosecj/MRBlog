3d1bf4936047ade19e7fcf3cc1feec121ffde2d3
Jose Carlos <josecarloscjj@gmail.com>
2022-10-15 14:20:38 -0300

    PostCard finish

--- frontend/src/components/BlogCard/index.tsx
+++ frontend/src/components/BlogCard/index.tsx
@@ -7,23 +7,22 @@ type Props = {
 };
 
 function BlogCard({ blog } : Props){
-
     return (
         <div className="container">
         <div className="mrblog-card-container">
           <div className="mrblog-card">
-          <Link to={`/post/${blog.id}`}>
-          <h2>{blog.title}</h2>
-          </Link>
-            <p className="mrblog-line">{blog.titleDescription}</p>
-            <p className="mrblog-date">Autor - {blog.user.name} | Categoria - {blog.category.name} | Criado em {blog.dateTime}</p>
+            <Link to={`/post/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            </Link>
+            <p className='mrblog-subtitle'>{blog.titleDescription}</p>
+            <p className="mrblog-line"></p>
+            <p className="mrblog-date">Criado em {blog.dateTime}</p>
             <div className="mrblog-card-description">
               <p>{blog.description}</p>
             </div>
-          </div>
+            </div>
         </div>
         </div>
     )
-
 }
 export default BlogCard;
--- frontend/src/components/PostCard/index.tsx
+++ frontend/src/components/PostCard/index.tsx
@@ -1,3 +1,4 @@
+import'./styles.css';
 import axios from 'axios';
 import { Link } from "react-router-dom";
 import { useEffect, useState } from 'react';
@@ -23,18 +24,24 @@ function PostCard({ postId } : Props) {
         <div className="mrblog-card-container">
           <div className="mrblog-card">
             <h2>{post?.title}</h2>
-            <p className="mrblog-line">{post?.titleDescription}</p>
-            <p className="mrblog-date">{post?.dateTime}</p>
+            <p className='mrblog-subtitle'>{post?.titleDescription}</p>
+            <p className="mrblog-line"></p>
+            <p className="mrblog-date">Criado em {post?.dateTime}</p>
             <div className="mrblog-card-description">
               <p>{post?.description}</p>
               <div className="mrblog-user-card">
                 <img className="mrblog-user-image" src={post?.user.urlImage} alt={post?.user.name} />
-                <p>{post?.user.name}</p>
+                <div className='mrblog-user-detail'>
+                <p>Autor(a): {post?.user.name}</p>
+                <p>Cadastrado desde: {post?.user.registrationDate}</p>
+                </div>
               </div>
-              <Link to='/'>
-                <div className="btn btn-primary mrblog-btn">Voltar</div>
-              </Link>
             </div>
+              <div className="btn btn-primary mrblog-btn">
+                <Link to='/'>
+                Voltar
+                </Link>
+              </div>
           </div>
         </div>
         </div>
