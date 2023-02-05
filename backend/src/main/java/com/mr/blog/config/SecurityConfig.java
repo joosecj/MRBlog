dce89b8f9a4dd7355b5f668ff41cc915cd578916
Jose Carlos <josecarloscjj@gmail.com>
2023-02-05 15:44:49 -0300

    test: cors config swagger nuvem

--- backend/src/main/java/com/mr/blog/config/SecurityConfig.java
+++ backend/src/main/java/com/mr/blog/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.mr.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
@@ -20,9 +25,14 @@ public class SecurityConfig {
   @Value("${cors.originPatterns:default}")
   private String corsOriginPatterns;
 
+  @Autowired
+  private Environment env;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.headers().frameOptions().disable();
+    if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+      http.headers().frameOptions().disable();
+    }
     http.cors().configurationSource(corsConfigurationSource());
     http.csrf().disable();
     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -34,6 +44,7 @@ public class SecurityConfig {
   CorsConfigurationSource corsConfigurationSource() {
     String[] origins = corsOriginPatterns.split(",");
     CorsConfiguration corsConfig = new CorsConfiguration();
+    corsConfig.setAllowedOrigins(Arrays.asList(origins));
     corsConfig.setAllowedOriginPatterns(Arrays.asList(origins));
     corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
     corsConfig.setAllowCredentials(true);
@@ -42,5 +53,13 @@ public class SecurityConfig {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", corsConfig);
     return source;
+  }
+
+  @Bean
+  FilterRegistrationBean<CorsFilter> corsFilter() {
+    FilterRegistrationBean<CorsFilter> bean
+            = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
   }
 }
