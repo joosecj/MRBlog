780f982a02363ea16c290a43976023e9c5bd12a9
Jose Carlos <josecarloscjj@gmail.com>
2023-03-11 16:51:05 -0300

    feat: [WIP] disable authentication JWT

--- backend/src/main/java/com/mr/blog/config/SecurityConfig.java
+++ backend/src/main/java/com/mr/blog/config/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -57,12 +55,12 @@ public class SecurityConfig {
                     .requestMatchers(
                             "/auth/signin",
                             "/auth/refresh/**",
-                            "/api-docs/**",
+                            "v3/api-docs/**",
                             "/swagger-ui/**",
                             "/h2-console/**"
                     ).permitAll()
                     .requestMatchers("/api/**")
-                    .authenticated()
+                    .permitAll()
             )
             .cors()
             .and()
